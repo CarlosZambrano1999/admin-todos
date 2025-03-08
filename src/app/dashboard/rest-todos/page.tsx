e4052d5fbb4f9978c41293a414bd5f90d2f8a4e0
Carlos Daniel Zambrano <cdanzamcol@gmail.com>
2025-03-07 23:03:01 -0600

    Sidebar & TopBar

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,5 +1,16 @@
-export default function RestTodosPage(){
-    return (
+import prisma from "@/lib/prisma";
+
+export const metada = {
+  title: "Listado de Todos",
+  description: "SEO title",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
+  return (
+    <div>
       <h1>Hello Page RestTodosPage</h1>
-    )
-  }
+      {JSON.stringify(todos)}
+    </div>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,72 @@
+import { CiLogout } from "react-icons/ci";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./SidebarItem";
+import { IoCalendarOutline, IoCheckboxOutline, IoListOutline } from "react-icons/io5";
+
+const menuItems = [
+    {
+        icon: <IoCalendarOutline />,
+        title: 'Dashboard',
+        path: '/dashboard'
+    },
+    {
+        icon: <IoCheckboxOutline />,
+        title: 'Rest Todos',
+        path: '/dashboard/rest-todos'
+    },
+    {
+        icon: <IoListOutline />,
+        title: 'Server actions',
+        path: '/dashboard/server-todos'
+    },
+]
+
+export const Sidebar = () => {
+  return (
+    <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+      <div>
+        <div className="-mx-6 px-6 py-4">
+          {/* TODO: Next/Link hacia dashboard */}
+          <Link href="/" title="home">
+            {/* Next/Image */}
+            <Image
+              src="https://cdn.worldvectorlogo.com/logos/nvidia.svg"
+              width={32}
+              height={32}
+              alt="tailus logo"
+            />
+          </Link>
+        </div>
+
+        <div className="mt-8 text-center">
+          {/* Next/Image */}
+          <Image
+            src="https://randomuser.me/api/portraits/men/1.jpg"
+            alt=""
+            width={150}
+            height={150}
+            className="m-auto rounded-full object-cover"
+          />
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
+            Cynthia J. Watts
+          </h5>
+          <span className="hidden text-gray-400 lg:block">Admin</span>
+        </div>
+
+        <ul className="space-y-2 tracking-wide mt-8">
+            { menuItems.map( item => (
+                <SidebarItem key={item.path} {...item}/>
+            ))}            
+        </ul>
+      </div>
+
+      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+          <CiLogout />
+          <span className="group-hover:text-gray-700">Logout</span>
+        </button>
+      </div>
+    </aside>
+  );
+};
